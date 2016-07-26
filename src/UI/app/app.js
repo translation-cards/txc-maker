8070e19da0626d6869792e7b4de1a7a5fe156377
Austin Garrard <agarrard@thoughtworks.com>
2016-07-26 14:50:19 -0500

    Austin - fix material design & angular interactions

--- src/UI/app/app.js
+++ src/UI/app/app.js
@@ -10,4 +10,27 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   //$locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/DeckImport'});
-}]);
+}])
+
+.run(function () {
+    var mdlUpgradeDom = false;
+    setInterval(function() {
+      if (mdlUpgradeDom) {
+        componentHandler.upgradeDom();
+        mdlUpgradeDom = false;
+      }
+    }, 200);
+
+    var observer = new MutationObserver(function () {
+      mdlUpgradeDom = true;
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+    /* support <= IE 10
+    angular.element(document).bind('DOMNodeInserted', function(e) {
+        mdlUpgradeDom = true;
+    });
+    */
+});
