3a09f951e6af6b818eea3d62a5cfdc03bb858396
Austin Garrard <agarrard@thoughtworks.com>
2016-07-29 21:50:49 -0500

    Austin - Add ngMaterial libraries to karma config

--- src/UI/karma.conf.js
+++ src/UI/karma.conf.js
@@ -8,6 +8,10 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-animate/angular-animate.min.js',
+      'bower_components/angular-aria/angular-aria.min.js',
+      'bower_components/angular-messages/angular-messages.min.js',
+      'bower_components/angular-material/angular-material.min.js',
 
       'app.js',
 
