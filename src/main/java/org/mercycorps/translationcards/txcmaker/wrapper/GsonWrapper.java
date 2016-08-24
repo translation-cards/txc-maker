49c818de654112cb4f2094f7424aa534571736e1
Austin Garrard <agarrard@thoughtworks.com>
2016-08-24 15:24:22 -0500

    Don't include errors field in serialized models when there are no errors

--- src/main/java/org/mercycorps/translationcards/txcmaker/wrapper/GsonWrapper.java
+++ src/main/java/org/mercycorps/translationcards/txcmaker/wrapper/GsonWrapper.java
@@ -1,6 +1,9 @@
 package org.mercycorps.translationcards.txcmaker.wrapper;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.mercycorps.translationcards.txcmaker.model.Card;
+import org.mercycorps.translationcards.txcmaker.model.CardSerializer;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nullable;
@@ -11,7 +14,9 @@ public class GsonWrapper {
     private final Gson gson;
 
     public GsonWrapper() {
-        gson = new Gson();
+        gson = new GsonBuilder()
+                .registerTypeAdapter(Card.class, new CardSerializer())
+                .create();
     }
 
     public String toJson(Object object) {
