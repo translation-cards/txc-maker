71cb295d42b8944ad26ce422dc448ae287c6bd0c
Austin Garrard <agarrard@thoughtworks.com>
2016-08-24 15:43:49 -0500

    Issue #32: Austin - Fix import

--- src/main/java/org/mercycorps/translationcards/txcmaker/model/DeckSerializer.java
+++ src/main/java/org/mercycorps/translationcards/txcmaker/model/DeckSerializer.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import org.mercycorps.translationcards.txcmaker.model.deck.Deck;
 
 import java.lang.reflect.Type;
 
